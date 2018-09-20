a6ebb21243bce629894be7725e47d16386b9c3c6
Patrick Biffle <pbiffle@gmail.com>
2018-09-20 17:47:43 -0600

    reorganized initial form, redirect NOT WORKING

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,23 +1,20 @@
 import React, { Component } from 'react'
 import '../styles/css/Login.css'
 
-
-
 class Login extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
 			user: {
 				username: '',
-				passwd: ''
+				password: ''
 			}
 		}
 	}
 	
-	loginPost = (e, object) => {
-		e.preventDefault()
+	loginPost = (object) => {
 		console.log(JSON.stringify(object))
-		fetch('http://localhost:3000/login',
+		fetch('http://localhost:3000/auth/login',
 		{
 			method: 'POST',
 			body: JSON.stringify(object), // data can be `string` or {object}!
@@ -29,18 +26,35 @@ class Login extends Component {
 			.then(resp => console.log(resp)
 			)}
 
+	createUserPost = (object) => {
+		fetch('http://localhost:3000/auth/signup',
+		{
+			method: 'POST',
+			body: JSON.stringify(object), // data can be `string` or {object}!
+			headers:
+				{
+				'Content-Type': 'application/json'
+				}
+		})
+			.then(resp => console.log(resp)
+	)}
+
 	render() {
 		return (
 			<div className="Login">
-				<form onSubmit={(e) => this.loginPost(e, this.state.user)}>
-				<label className='label'>USERNAME<input className='input' value={this.state.user.username} onChange={(e) => this.setState({
-					user: {... this.state.user, username: e.target.value}
-				})}/></label>
-				<label className='label'>PASSWORD<input value={this.state.user.password} onChange={(e) => this.setState({ user: {... this.state.user, passwd: e.target.value}})}type='password' className='input'/></label>
-				<input className='button-green' type='submit' value='LOGIN' />
-				<input className='button-blue' type='submit' value='CREATE NEW USER' />
-				<input className='button-blue' type='submit' value='GUEST' />
-				</form>
+				<div className='form'>
+					<label className='label'>USERNAME
+						<input className='input' value={this.state.user.username} onChange={(e) => 
+							this.setState({user: {... this.state.user, username: e.target.value}
+						})}/>
+					</label>
+					<label className='label'>PASSWORD
+						<input value={this.state.user.password} onChange={(e) => 
+							this.setState({ user: {... this.state.user, password: e.target.value}})}type='password' className='input'/>
+					</label>
+					<button className='button-green' onClick={() => this.loginPost(this.state.user)}>LOGIN</button>
+					<button className='button-blue' onClick={() => this.createUserPost(this.state.user)}>CREATE NEW USER</button>
+				</div>
 			</div>
 		)
 	}
